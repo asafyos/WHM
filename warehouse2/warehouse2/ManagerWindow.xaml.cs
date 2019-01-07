ba32b306ea17c4cedf20b1552cdfbc7c9bdc64f4
asafyos <asafsaf3@gmail.com>
2019-01-08 00:55:03 +0200

    Set the storekeeper DDL

--- warehouse2/warehouse2/ManagerWindow.xaml.cs
+++ warehouse2/warehouse2/ManagerWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,10 +18,35 @@ namespace warehouse2 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class ManagerWindow : Window {
+    public partial class ManagerWindow : Window, INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string property) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        SharedData shareDataIns;
+        public SharedData ShareDataIns {
+            get { return this.shareDataIns; }
+            set {
+                this.shareDataIns = value;
+                OnPropertyChanged("ShareDataIns");
+            }
+        }
+        public ObservableCollection<MemberDets> StorekeepersList {
+            get {
+                ObservableCollection<MemberDets> list = new ObservableCollection<MemberDets>(SharedData.GetInstans().StorekeepersList);
+                list.Insert(0, new MemberDets { MemberName = "בחר מחסנאי", MemberID = -1, });
+                return list;
+            }
+        }
+
         public ManagerWindow() {
             InitializeComponent();
             this._UserName.Focus();
+            ShareDataIns = SharedData.GetInstans();
         }
 
         private void button_Click(object sender, RoutedEventArgs e) {
@@ -38,7 +65,7 @@ namespace warehouse2 {
         }
 
         private void _Password_KeyUp(object sender, KeyEventArgs e) {
-          if (e.Key == Key.Enter) {
+            if (e.Key == Key.Enter) {
                 if (this.tryLogin()) {
                     MainWindow.mainWin.ManagerIn = true;
                     SharedData.GetInstans().CurrentManager = new ManagerDets { UserName = this._UserName.Text, Password = this._Password.Password };
